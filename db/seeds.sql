e358a22eff0559a1abfdd041414cfee72d1547ee
Sara Baqla <missatrox44@gmail.com>
2022-07-29 22:27:16 -0500

    added seeds, role and employee bugs

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,3 +4,17 @@ VALUES ("Sales"),
        ("Finance"),
        ("Engineering");
 
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 1),
+       ("Salesperson", 80000, ),
+       ("Lead Engineer", 150000, 4),
+       ("Software Engineer", 120000, 4),
+       ("Account Manager", 160000, 3),
+       ("Accountant", 125000, 3),
+       ("Legal Team Lead", 250000, 2),
+       ("Lawyer", 190000, 2);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Sara, Baqla, Software Engineer")
+      
