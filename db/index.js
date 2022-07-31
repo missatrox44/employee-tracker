049becbd7da1507b3e3f14a9c8e782ebca81e78f
Sara Baqla <missatrox44@gmail.com>
2022-07-31 14:17:38 -0500

    updated viewAllEmployees function

--- db/index.js
+++ db/index.js
@@ -16,7 +16,7 @@ class EmployeeDB {
   
   //include id, firstName, lastName, job title, dept, salary, manager reports to
   viewAllEmployees() {
-    return this.connection.promise().query("SELECT * FROM EMPLOYEES")
+    return this.connection.promise().query("SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.dept AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees LEFT JOIN roles on employees.role_id = roles.id LEFT JOIN departments on roles.department_id = departments.id LEFT JOIN employees manager on manager.id = employees.manager_id")
   }
 }
 
--- index.js
+++ index.js
@@ -74,4 +74,5 @@ const viewAllDepartments = () => {
 
 //ADD DEPARTMENT
 
+
 startProgram();
