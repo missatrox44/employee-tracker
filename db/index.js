fc412a527a3540be1e2c2091c9f9f43f7dd84261
Sara Baqla <missatrox44@gmail.com>
2022-08-01 21:37:55 -0500

    updateEmp bugs, started addRoles function

--- db/index.js
+++ db/index.js
@@ -2,35 +2,48 @@ const connection = require('./connection');
 const inquirer = require('inquirer');
 
 class EmployeeDB {
-  constructor (connection){
+  constructor(connection) {
     this.connection = connection;
   }
   //include id, name
   viewAllDepartments() {
     return this.connection.promise().query("SELECT * FROM DEPARTMENTS")
   }
-  
+
   //include id, job title, dept, salary
   viewAllRoles() {
     return this.connection.promise().query("SELECT roles.id, roles.title, departments.dept, roles.salary FROM roles INNER JOIN departments ON roles.department_id=departments.id")
   }
-  
+
   //include id, firstName, lastName, job title, dept, salary, manager reports to
   viewAllEmployees() {
     return this.connection.promise().query("SELECT employees.id, employees.first_name, employees.last_name, roles.title, departments.dept AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees LEFT JOIN roles on employees.role_id = roles.id LEFT JOIN departments on roles.department_id = departments.id LEFT JOIN employees manager on manager.id = employees.manager_id")
   }
 
   //add new dept to dept table w/ inquirer
-  addDept(input) {
+  insertDept(input) {
     return this.connection.promise().query("INSERT INTO departments (dept) VALUES (?)", input)
-    }
+  }
 
+  //add new roles to role table w/ inquirer
+  insertRole(input) {
+    return this.connection.promise().query("INSERT INTO roles (title) VALUES (?)", input)
+  }
 
+  //produce of employee names only
   callEmployees() {
     return this.connection.promise().query("SELECT employees.id, employees.first_name, employees.last_name FROM employees")
-    }
-    
   }
+
+  //produce list of role titles only
+  callRoles() {
+    return this.connection.promise().query("SELECT roles.id, roles.title FROM roles")
+  }
+
+  update(input) {
+    return this.connection.promise().query("UPDATE roles (title) VALUES (?)", input)
+  }
+}
 
 
 module.exports = new EmployeeDB(connection);
--- index.js
+++ index.js
@@ -19,94 +19,132 @@ const mainMenu = [
 
 const startProgram = () => {
   inquirer.prompt(mainMenu)
-  .then(decision => {
-    if(decision.choice === 'View All Employees'){
-      // console.log('view employees function');
-      viewAllEmployees();
-    } else if (decision.choice === 'Add Employee'){
-      console.log('add employee function');
-    } else if (decision.choice === 'Update Employee Role'){
-      // console.log('update employee function');
-      updateEmp();
-    } else if (decision.choice === 'View All Roles'){
-      // console.log('view all roles function');
-      viewAllRoles();
-    } else if (decision.choice === 'Add Roles'){
-      console.log('add roles function');
-    } else if (decision.choice === 'View All Departments'){
-      // console.log('view all departments function');
-      viewAllDepartments();
-    } else if (decision.choice === 'Add Department'){
-      // console.log('add department function');
-      addDept();
-    } else {
-      console.log('Thanks for stopping by!');
-    }
-  })
+    .then(decision => {
+      if (decision.choice === 'View All Employees') {
+        viewEmployeeInfo();
+      } else if (decision.choice === 'Add Employee') {
+        console.log('Function under construction');
+        startProgram();
+        //addEmployee function
+      } else if (decision.choice === 'Update Employee Role') {
+        console.log('Function under construction');
+        // updateEmp();
+        startProgram();
+      } else if (decision.choice === 'View All Roles') {
+        viewRoles();
+      } else if (decision.choice === 'Add Roles') {
+        addNewRole();
+      } else if (decision.choice === 'View All Departments') {
+        viewDepts();
+      } else if (decision.choice === 'Add Department') {
+        addDept();
+      } else {
+        console.log('Thanks for stopping by!');
+      }
+    })
 }
 
-const viewAllEmployees = () => {
+
+const viewEmployeeInfo = () => {
   EmployeeDB.viewAllEmployees()
-  .then(([rows]) => {
-    console.table(rows)
-    startProgram();
-  })
+    .then(([rows]) => {
+      console.table(rows)
+      startProgram();
+    })
 }
 
 //ADD EMPLOYEE 
 
 
-//UPDATE EMPLOYEE ROLE
-const updateEmp = () => {
-    EmployeeDB.callEmployees()
-    .then(([rows]) => {
-      const employeeChoices = rows.map(employee => {
-        return {
-          name: `${employee.first_name} ${employee.last_name}`,
-          value: employee.id
-        }
-      })
-    inquirer.prompt([
-      {
-        type: 'list',
-        name: 'chosenEmp',
-        message: 'Which employees role do you want to update?',
-        choices: employeeChoices
-      }
-    ])
-   .then ((answer) => {
-    EmployeeDB.addDept(answer.chosenEmp)  
-    console.log(`Selected ${answer.chosenEmp} to update.`)
-  })
-  .then(() => {
-    //inquirer- WHich role would you like to assign this person
-    console.log('Which role would you like to assign this person?');
-    startProgram();
-   })
-})
-}
+// //UPDATE EMPLOYEE ROLE
+// const updateEmp = () => {
+//   EmployeeDB.callEmployees()
+//     .then(([rows]) => {
+//       const employeeChoices = rows.map(employee => {
+//         return {
+//           name: `${employee.first_name} ${employee.last_name}`,
+//           value: employee.first_name
+//         }
+//       })
+//       inquirer.prompt([
+//         {
+//           type: 'list',
+//           name: 'chosenEmp',
+//           message: 'Which employees role do you want to update?',
+//           choices: employeeChoices
+//         }
+//       ])
+//         .then((answer) => {
+//           EmployeeDB.insertDept(answer.chosenEmp)
+//           // console.log(`Selected ${answer.chosenEmp} to update.`)
+//         })
+//         .then(() => {
+//           EmployeeDB.callRoles()
+//             .then(([rows]) => {
+//               const roleChoices = rows.map(role => {
+//                 return {
+//                   title: `${role.title}`,
+//                   value: role.title
+//                 }
+//               })
+//               inquirer.prompt([
+//                 {
+//                   type: 'list',
+//                   name: 'chosenRole',
+//                   message: 'Which role would you like to assign this person?',
+//                   choices: roleChoices
+//                 }
+//               ])
+//                 .then((answer) => {
+//                   console.log(`Assigned the role ${answer.chosenRole} to ${answer.chosenEmp}`)
+//                 })
+//             })startProgram();
+//         })
+//     })
+// }
+
 
 
 
 //VIEW ALL ROLES
-const viewAllRoles = () => {
+const viewRoles = () => {
   EmployeeDB.viewAllRoles()
-  .then(([rows]) => {
-    console.table(rows)
-    startProgram();
-  })
+    .then(([rows]) => {
+      console.table(rows)
+      startProgram();
+    })
 }
+
+//ADD ROLES question
+const addRolesQ = [
+  {
+    type: 'input',
+    name: 'newRole',
+    message: 'What is the name of the role?',
+  }
+]
 
 //ADD ROLES
+const addNewRole = () => {
+  inquirer.prompt(addRolesQ)
+    .then((answer) => {
+      EmployeeDB.insertRole(answer.newRole)
+      console.log(`Added ${answer.newRole} to Employee Database.`)
+    })
+    .then(() => {
+      startProgram();
+    })
+}
 
 //VIEW ALL DEPARTMENTS
-const viewAllDepartments = () => {
+const viewDepts = () => {
   EmployeeDB.viewAllDepartments()
-  .then(([rows]) => {
-    console.table(rows)
-    startProgram();
-  })
+    .then(([rows]) => {
+      console.table(rows)
+      startProgram();
+    })
 }
+
 
 //ADD DEPARTMENT question
 const addDeptQ = [
@@ -120,13 +158,13 @@ const addDeptQ = [
 //ADD DEPARTMENT
 const addDept = () => {
   inquirer.prompt(addDeptQ)
-  .then ((answer) => {
-    EmployeeDB.addDept(answer.newDept)  
-    console.log(`Added ${answer.newDept} to Employee Database.`)
-  })
-  .then(() => {
-    startProgram();
-  })
+    .then((answer) => {
+      EmployeeDB.insertDept(answer.newDept)
+      console.log(`Added ${answer.newDept} to Employee Database.`)
+    })
+    .then(() => {
+      startProgram();
+    })
 }
 
 
